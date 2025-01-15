0c1ab3a75a9cc97ab614823fa0380b663e9e4417
Alex Spieslechner <alexander.spieslechner@dynatrace.com>
2025-01-15 18:56:56 +0100

    feat: use yazi instead of netrw

--- .config/nvim/lua/config/plugins/yazi.lua
+++ .config/nvim/lua/config/plugins/yazi.lua
@@ -6,30 +6,23 @@ return {
     keys = {
       -- 👇 in this section, choose your own keymappings!
       {
-	"<leader>-",
-	"<cmd>Yazi<cr>",
-	desc = "Open yazi at the current file",
+        "<leader>-",
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
       },
       {
-	-- Open in the current working directory
-	"<leader>cw",
-	"<cmd>Yazi cwd<cr>",
-	desc = "Open the file manager in nvim's working directory" ,
-      },
-      {
-	-- NOTE: this requires a version of yazi that includes
-	-- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-	'<c-up>',
-	"<cmd>Yazi toggle<cr>",
-	desc = "Resume the last yazi session",
+        -- Open in the current working directory
+        "<leader>cw",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open the file manager in nvim's working directory" ,
       },
     },
     ---@type YaziConfig
     opts = {
       -- if you want to open yazi instead of netrw, see below for more info
-      open_for_directories = false,
+      open_for_directories = true,
       keymaps = {
-	show_help = '<f1>',
+        show_help = '<f1>',
       },
     },
   }
