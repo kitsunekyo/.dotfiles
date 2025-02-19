80dac9072f89db6ee12deaf8e82a733d5e99f5b2
Alex Spieslechner <alexander.spieslechner@dynatrace.com>
2025-02-19 11:56:40 +0100

    feat: remove hunk toggle

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -38,8 +38,7 @@ return {
         end, { desc = "git [s]tage hunk" })
         map("v", "<leader>hr", function()
           gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-        end, { desc = "git [r]eset hunk" })
-        -- normal mode
+        end, { desc = "git [r]eset hunk" }) -- normal mode
         map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "git [s]tage hunk" })
         map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "git [r]eset hunk" })
         map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "git [S]tage buffer" })
@@ -52,8 +51,7 @@ return {
           gitsigns.diffthis("@")
         end, { desc = "git [D]iff against last commit" })
         -- Toggles
-        map("n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "[T]oggle git show [b]lame line" })
-        map("n", "<leader>tD", gitsigns.preview_hunk_inline, { desc = "[T]oggle git show [D]eleted" })
+        map("n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "Toggle inline git blame" })
       end,
     },
   },
