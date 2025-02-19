b272db5a7a14770bec848dc449ea4e4d574cad7f
Alex Spieslechner <alexander.spieslechner@dynatrace.com>
2025-02-19 15:57:53 +0100

    feat: add <C-f> as autocomplete map

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -10,21 +10,18 @@ return {
   },
   {
     "saghen/blink.cmp",
+    version = "*",
     dependencies = "rafamadriz/friendly-snippets",
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
     opts = {
-      -- 'default' for mappings similar to built-in completion
-      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-      -- See the full "keymap" documentation for information on defining your own keymap.
-      keymap = { preset = "default" },
+      keymap = { preset = "default", ["<C-f>"] = { "accept" } },
       appearance = {
-        use_nvim_cmp_as_default = true,
         nerd_font_variant = "mono",
       },
       sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
+        default = { "lsp", "path", "buffer" },
+      },
+      cmdline = {
+        enabled = false,
       },
     },
     opts_extend = { "sources.default" },
