d68811d7d906ff8d4df4278dbdd75ca02606cd47
Alex Spieslechner <alexander.spieslechner@dynatrace.com>
2025-02-23 00:13:28 +0100

    feat: config bufferline style

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -3,6 +3,14 @@ return {
     "akinsho/bufferline.nvim",
     version = "*",
     dependencies = "nvim-tree/nvim-web-devicons",
-    opts = {},
+    config = function()
+      local bufferline = require("bufferline")
+      bufferline.setup({
+        options = {
+          style_preset = bufferline.style_preset.minimal,
+          buffer_close_icon = "󰅖",
+        },
+      })
+    end,
   },
 }
