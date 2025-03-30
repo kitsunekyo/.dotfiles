d170a2b898f02c00723c83713c20c45a2d0c3cab
Alex Spieslechner <alexander.spieslechner@dynatrace.com>
2025-03-30 12:49:31 +0200

    feat: improve fold

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -34,10 +34,25 @@ vim.o.smartindent = true
 vim.o.autoindent = true
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
-vim.o.foldmethod = "indent"
-vim.o.foldlevel = 99
-vim.o.foldlevelstart = 99
 vim.o.breakindent = true
+
+-- fold
+vim.o.foldlevel = 99
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+-- Prefer LSP folding if client supports it
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client:supports_method("textDocument/foldingRange") then
+      local win = vim.api.nvim_get_current_win()
+      vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+    end
+  end,
+})
+vim.o.foldtext = ""
+vim.opt.foldcolumn = "0"
+vim.opt.fillchars:append({ fold = " " })
 
 -- completion
 vim.o.completeopt = "menuone,noinsert,noselect"
