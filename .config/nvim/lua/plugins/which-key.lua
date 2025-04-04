915a3f65df3a4bb8e124b8f61a41e63c0d83b67a
Alex Spieslechner <alexander.spieslechner@dynatrace.com>
2025-04-05 00:13:40 +0200

    feat: remove leader w as workspace whichkey

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -14,7 +14,6 @@ return {
         { "<leader>b", group = "[B]uffer" },
         { "<leader>r", group = "[R]ename" },
         { "<leader>s", group = "[S]earch" },
-        { "<leader>w", group = "[W]orkspace" },
         { "<leader>t", group = "[T]oggle" },
         { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
       },
