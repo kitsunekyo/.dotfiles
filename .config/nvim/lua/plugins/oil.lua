c4ab7c6be2ef4eb8bc844c0b379e31591d997ebc
Alex Spieslechner <alexander.spieslechner@dynatrace.com>
2025-04-13 14:00:23 +0200

    feat: update oil config

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,23 +2,24 @@ return {
   {
     "stevearc/oil.nvim",
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    config = function()
-      require("oil").setup({
-        default_file_explorer = true,
-        columns = {
-          "icon",
-        },
-        delete_to_trash = true,
-        skip_confirm_for_simple_edits = true,
-        view_options = {
-          show_hidden = true,
-          natural_order = true,
-          is_always_hidden = function(name, _)
-            return name == ".git"
-          end,
-        },
-      })
-      vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
-    end,
+    lazy = false,
+    opts = {
+      default_file_explorer = true,
+      columns = {
+        "icon",
+      },
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+        natural_order = true,
+        is_always_hidden = function(name, _)
+          return name == ".git"
+        end,
+      },
+    },
+    keys = {
+      { "-", "<cmd>Oil<cr>", { desc = "Open parent directory" } },
+    },
   },
 }
