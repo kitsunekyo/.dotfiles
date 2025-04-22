79e542d1ef0844f45038bcab89b104907cabf948
Alex Spieslechner <alexander.spieslechner@dynatrace.com>
2025-04-22 20:43:37 +0200

    feat: use onedark theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,16 @@
 return {
   {
-    "catppuccin/nvim",
+    -- "catppuccin/nvim",
     -- "folke/tokyonight.nvim",
     -- "projekt0n/github-nvim-theme",
-    -- "olimorris/onedarkpro.nvim",
+    "olimorris/onedarkpro.nvim",
     lazy = false,
     priority = 1000,
     config = function()
       -- vim.cmd.colorscheme("tokyonight-storm")
-      vim.cmd.colorscheme("catppuccin-mocha")
+      -- vim.cmd.colorscheme("catppuccin-mocha")
       -- vim.cmd.colorscheme("github_dark")
-      -- vim.cmd.colorscheme("onedark")
+      vim.cmd.colorscheme("onedark")
     end,
   },
 }
