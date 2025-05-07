c774fac4edd044ba90f44229d3280eb2c4956f2f
Alex Spieslechner <alexander.spieslechner@dynatrace.com>
2025-05-07 21:54:12 +0200

    feat: move folding to lsp file

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -112,6 +112,11 @@ return {
               end,
             })
           end
+
+          if client:supports_method("textDocument/foldingRange") then
+            local win = vim.api.nvim_get_current_win()
+            vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+          end
         end,
       })
     end,
