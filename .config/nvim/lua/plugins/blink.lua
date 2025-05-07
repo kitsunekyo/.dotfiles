eef2a8e342f9d2aa1c5a460259cc2b634cb047df
Alex Spieslechner <alexander.spieslechner@dynatrace.com>
2025-05-07 14:58:17 +0200

    feat: update blink.cmp

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -8,6 +8,20 @@ return {
     opts = {
       keymap = { preset = "default", ["<C-f>"] = { "accept" }, ["<CR>"] = { "accept", "fallback" } },
       cmdline = { enabled = false },
+      completion = {
+        ghost_text = { enabled = true },
+        menu = {
+          draw = {
+            columns = {
+              { "kind_icon", "label", gap = 1 },
+              { "label_description" },
+            },
+          },
+        },
+      },
+      appearance = {
+        nerd_font_variant = "mono",
+      },
     },
   },
 }
