e5c111c857aa7ad62347d5ff6195dcd48e353ce5
Alex Spieslechner <alexander.spieslechner@dynatrace.com>
2025-05-10 21:42:53 +0200

    feat: refactor lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -13,6 +13,22 @@ return {
       "saghen/blink.cmp",
     },
     config = function()
+      -- diagnostics
+      vim.diagnostic.config({
+        severity_sort = true,
+        -- float = { border = "rounded", source = "if_many" },
+        -- underline = { severity = vim.diagnostic.severity.ERROR },
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = "󰅚",
+            [vim.diagnostic.severity.WARN] = "󰀪",
+            [vim.diagnostic.severity.INFO] = "󰋽",
+            [vim.diagnostic.severity.HINT] = "󰌶",
+          },
+        },
+        virtual_text = true,
+      })
+
       -- lsp manager
       require("mason").setup({
         ui = {
@@ -70,63 +86,65 @@ return {
         },
       })
 
-      vim.diagnostic.config({
-        severity_sort = true,
-        -- float = { border = "rounded", source = "if_many" },
-        -- underline = { severity = vim.diagnostic.severity.ERROR },
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = "󰅚",
-            [vim.diagnostic.severity.WARN] = "󰀪",
-            [vim.diagnostic.severity.INFO] = "󰋽",
-            [vim.diagnostic.severity.HINT] = "󰌶",
-          },
-        },
-        virtual_text = true,
-      })
-
       vim.api.nvim_create_autocmd("LspAttach", {
-        desc = "configure lsp features",
+        desc = "setup lsp keybinds",
         callback = function(event)
           local function map(keys, cb, desc)
             vim.keymap.set("n", keys, cb, { buffer = event.buf, noremap = true, desc = desc or "" })
           end
 
-          -- stylua: ignore start
           -- lsp-defaults
           -- - "grn" is mapped in Normal mode to |vim.lsp.buf.rename()|
           -- - "gra" is mapped in Normal and Visual mode to |vim.lsp.buf.code_action()|
           -- - "grr" is mapped in Normal mode to |vim.lsp.buf.references()|
           -- - "gri" is mapped in Normal mode to |vim.lsp.buf.implementation()|
           -- - "gO" is mapped in Normal mode to |vim.lsp.buf.document_symbol()|
+
+          -- stylua: ignore start
           map("<leader>tD", function() vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end, "Inline diagnostics")
           map("üd", function() vim.diagnostic.jump({ count=-1, float=true, wrap=true }) end, "Previous diagnostic")
           map("+d", function() vim.diagnostic.jump({ count=1, float=true, wrap=true }) end, "Next diagnostic")
           map("<C-k>", vim.lsp.buf.signature_help, "Show signature_help")
           -- stylua: ignore end
+        end,
+      })
 
+      vim.api.nvim_create_autocmd("LspAttach", {
+        desc = "use lsp folding",
+        callback = function(event)
           local client = assert(vim.lsp.get_client_by_id(event.data.client_id))
 
-          if client:supports_method("textDocument/documentHighlight") then
-            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-              desc = "Highlight",
-              callback = function(_)
-                vim.lsp.buf.document_highlight()
-              end,
-            })
-
-            vim.api.nvim_create_autocmd("CursorMoved", {
-              desc = "Remove Highlight",
-              callback = function(_)
-                vim.lsp.buf.clear_references()
-              end,
-            })
+          if not client:supports_method("textDocument/foldingRange") then
+            return
           end
 
-          if client:supports_method("textDocument/foldingRange") then
-            local win = vim.api.nvim_get_current_win()
-            vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+          local win = vim.api.nvim_get_current_win()
+          vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        desc = "Highlight symbol",
+        callback = function(event)
+          local client = assert(vim.lsp.get_client_by_id(event.data.client_id))
+
+          if not client:supports_method("textDocument/documentHighlight") then
+            return
           end
+
+          vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+            desc = "Highlight",
+            callback = function(_)
+              vim.lsp.buf.document_highlight()
+            end,
+          })
+
+          vim.api.nvim_create_autocmd("CursorMoved", {
+            desc = "Remove Highlight",
+            callback = function(_)
+              vim.lsp.buf.clear_references()
+            end,
+          })
         end,
       })
     end,
