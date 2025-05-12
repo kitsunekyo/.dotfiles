0d428e8e1c0b6fa76b9028881dba4492f711e066
Alex Spieslechner <alexander.spieslechner@dynatrace.com>
2025-05-12 11:41:13 +0200

    feat: dont autoinstall ts_ls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -43,7 +43,6 @@ return {
       -- automatically enables installed lsps
       require("mason-lspconfig").setup({ ensure_installed = {
         "lua_ls",
-        "ts_ls",
         "eslint",
       } })
 
