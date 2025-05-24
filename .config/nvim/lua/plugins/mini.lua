248b49aaa2f4c4dcf45979ec929ef89a5f628c1a
Alex Spieslechner <alexander.spieslechner@dynatrace.com>
2025-05-24 21:51:02 +0200

    use win config

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -15,8 +15,14 @@ return {
       require("mini.pairs").setup()
       require("mini.jump").setup()
       require("mini.cursorword").setup()
-      require("mini.notify").setup()
-      vim.notify = require("mini.notify").make_notify()
+      -- require("mini.notify").setup()
+      -- vim.notify = require("mini.notify").make_notify()
+      local win_config = function()
+        local has_statusline = vim.o.laststatus > 0
+        local pad = vim.o.cmdheight + (has_statusline and 1 or 0)
+        return { anchor = "SE", col = vim.o.columns, row = vim.o.lines - pad }
+      end
+      require("mini.notify").setup({ window = { config = win_config } })
 
       local indent = require("mini.indentscope")
       indent.setup({})
