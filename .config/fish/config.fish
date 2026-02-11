78d8db9c26cfee7437fc32f3336776a4a4407464
Alex Spieslechner <alexander.spieslechner@dynatrace.com>
2026-02-11 12:12:24 +0100

    feat: add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,21 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+export EDITOR="nvim"
+export SUDO_EDITOR="nvim"
+export VISUAL="nvim"
+
+export FZF_DEFAULT_COMMAND="fd --type f"
+export FZF_DEFAULT_OPTS="--layout=default --border=rounded"
+
+# if [[ $(uname) == "Linux" ]]; then
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# fi
+
+atuin init fish --disable-up-arrow | source
+
